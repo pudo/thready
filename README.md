c12a1bfaf242b83e372341d73044fb0bc3ae7656
Friedrich Lindenberg <friedrich@pudo.org>
2013-06-29 00:02:44 +0200

    Initial checking

---
+++ README.md
@@ -0,0 +1,4 @@
+thready is a simple module for a multi-threading function that I use 
+in multiple scripts. Nothing to see here.
+
+Licensed under [MIT terms](http://opensource.org/licenses/MIT).
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='thready',
+    version='0.1',
+    description="Simple wrapper for threaded execution."
+    long_description="",
+    classifiers=[],
+    keywords='threading',
+    author='Friedrich Lindenberg',
+    author_email='friedrich@pudo.org',
+    url='http://github.com/pudo/thready',
+    license='MIT',
+    py_modules=['thready'],
+    zip_safe=False,
+    install_requires=[
+    ],
+    tests_require=[],
+    entry_points=""" """,
+)
