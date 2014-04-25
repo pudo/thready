e16fc450e8040aa3d5f03d959c3dd5bfe96b9d6e
Thomas Levine <_@thomaslevine.com>
2014-04-25 15:50:20 +0000

    test threading

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-def test_import():
+import thready
+
+def test_threaded():
     import thready
+    results = set()
+    def f(i):
+        results.add(i)
+    thready.threaded(range(10), f)
+    assert results == set(range(10))
