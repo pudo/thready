bf71b6e73a971079362d45fe7a291618701e0d00
Friedrich Lindenberg <friedrich@pudo.org>
2014-05-24 09:40:26 +0200

    up the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='thready',
-    version='0.1.3',
+    version='0.1.4',
     description="Simple wrapper for threaded execution.",
     long_description="",
     classifiers=[],
